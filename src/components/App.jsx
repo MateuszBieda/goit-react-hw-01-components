e6607051c220b998352c433e14093d5c34649129
MateuszBieda <mateusbieda@gmail.com>
2023-05-16 23:00:13 +0200

    component_Statisctics_update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-import {Profile} from 'components/Profile/Profile.jsx';
-import user from 'Data/user.json'; 
+import { Profile } from 'components/Profile/Profile.jsx';
+import { Statistics } from 'components/Statistics/Statistics.jsx';
+import user from 'Data/user.json';
+import data from 'Data/data.json';
 
 export const App = () => {
   return (
     <div>
-     <Profile
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-    />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
